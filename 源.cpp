fee193f181f17f371454ebcbf4636bc5a4c2932e
Hb-key <44340302+Hb-key@users.noreply.github.com>
2018-12-09 23:28:20 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,18 +1,33 @@
 
-//两数之和
+//1.两数之和
+//class Solution {
+//public:
+//	vector<int> twoSum(vector<int>& nums, int target) {
+//		vector<int> result(2, -1);
+//		for (int i = 0; i<nums.size(); i++)
+//			for (int j = i + 1; j<nums.size(); j++)
+//			{
+//				if (nums[i] + nums[j] == target)
+//				{
+//					result[0] = i;
+//					result[1] = j;
+//					return result;
+//				}
+//			}
+//	}
+//};
+
+//7.整数反转
 class Solution {
 public:
-	vector<int> twoSum(vector<int>& nums, int target) {
-		vector<int> result(2, -1);
-		for (int i = 0; i<nums.size(); i++)
-			for (int j = i + 1; j<nums.size(); j++)
-			{
-				if (nums[i] + nums[j] == target)
-				{
-					result[0] = i;
-					result[1] = j;
-					return result;
-				}
-			}
+	int reverse(int x) {
+		if (x > 0)
+		{
+			mol(n);
+		}
+	}
+	int mol(int n)
+	{
+
 	}
 };
