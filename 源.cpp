683d573e97592263886fd0f407102b60631e2dbd
Hb-key <44340302+Hb-key@users.noreply.github.com>
2018-12-13 22:44:59 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -17,27 +17,76 @@
 //};
 
 //7.整数反转
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//int reverse(int x) {
+//	int ret = 0;
+//	while (x)
+//	{
+//		ret = 10 * ret + x % 10;
+//		x = x / 10;
+//	}
+//	if (x <(-2 ^ 31) || x >(2 ^ 31 - 1))
+//	{
+//		return 0;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = reverse(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
 
-int reverse(int x) {
-	int ret = 0;
-	while (x)
-	{
-		ret = 10 * ret + x % 10;
-		x = x / 10;
+//9.回文数
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		String tempX = String.valueOf(x);
+		if (x < 0)
+		{
+			return false;
+		}
+		else
+			if (x > 0)
+			{
+				int i = 0;
+				int j = tempX.length() - 1;
+				if (i == j)
+				{
+					return true;
+				}
+				while (i != j)
+				{
+					if (i == j + 1)
+					{
+						break;
+					}
+					else if (tempX.charAt(i) == tempX.charAt(j))
+					{
+						i++;
+						j--;
+					}
+					else
+					{
+						break;
+					}
+				}
+				if (i == j || i>j)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else {
+				return true;
+			}
 	}
-	if (x <(-2 ^ 31) || x >(2 ^ 31 - 1))
-	{
-		return 0;
-	}
-	return ret;
-}
-int main()
-{
-	int n = 0;
-	scanf("%d", &n);
-	int ret = reverse(n);
-	printf("%d\n", ret);
-	return 0;
-}
+};
