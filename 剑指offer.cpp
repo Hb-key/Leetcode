b5d95d45539a6cb44b405e5e5c82dd415b442146
Hb-key <44340302+Hb-key@users.noreply.github.com>
2019-01-22 16:24:18 +0800

    Add files via upload

--- 剑指offer.cpp
+++ 剑指offer.cpp
@@ -1,46 +1,65 @@
-//1.二维数组中的查找
-class Solution {
-public:
-	bool Find(int target, vector<vector<int> > array) {
-		if (array.size() < 0)
-			return false;
-		int i = 0;
-		int j = array[0].size() - 1;
-		while (j >= 0 && i < array.size())
-		{
-			if (target == array[i][j])
-				return true;
-			else if (target > array[i][j])
-				i++;
-			else if (target < array[i][j])
-				j--;
-		}
-		return false;
-	}
-};
-//2.空格替换
-class Solution {
-public:
-	void replaceSpace(char *str, int length) {
-		int blank = 0;
-		for (int i = 0; i < length; i++)
-		{
-			if (str[i] == ' ')
-				blank++;
-		}
-		int j = length + blank * 2 - 1;
-		for (int i = length - 1; i >= 0; i--)
-		{
-			if (str[i] == ' ')
-			{
-				str[j--] = '0';
-				str[j--] = '2';
-				str[j--] = '%';
-			}
-			else
-			{
-				str[j--] = str[i];
-			}
-		}
-	}
-};
+////1.二维数组中的查找
+//class Solution {
+//public:
+//	bool Find(int target, vector<vector<int> > array) {
+//		if (array.size() < 0)
+//			return false;
+//		int i = 0;
+//		int j = array[0].size() - 1;
+//		while (j >= 0 && i < array.size())
+//		{
+//			if (target == array[i][j])
+//				return true;
+//			else if (target > array[i][j])
+//				i++;
+//			else if (target < array[i][j])
+//				j--;
+//		}
+//		return false;
+//	}
+//};
+////2.空格替换
+//class Solution {
+//public:
+//	void replaceSpace(char *str, int length) {
+//		int blank = 0;
+//		for (int i = 0; i < length; i++)
+//		{
+//			if (str[i] == ' ')
+//				blank++;
+//		}
+//		int j = length + blank * 2 - 1;
+//		for (int i = length - 1; i >= 0; i--)
+//		{
+//			if (str[i] == ' ')
+//			{
+//				str[j--] = '0';
+//				str[j--] = '2';
+//				str[j--] = '%';
+//			}
+//			else
+//			{
+//				str[j--] = str[i];
+//			}
+//		}
+//	}
+//};
+//3.斐波那契数列
+//class Solution {
+//public:
+//	int Fibonacci(int n) {
+//		if (n == 1 || n == 2)
+//		{
+//			return 1;
+//		}
+//		else if (n>2)
+//		{
+//			return (Fibonacci(n - 1) + Fibonacci(n - 2));
+//		}
+//		else
+//		{
+//			return 0;
+//		}
+//	}
+//};
+
